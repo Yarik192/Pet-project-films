cf8a8a32d3c390a57b838ad99b38c40a0ae440b7
Danil1024 <danaiek2003@mail.ru>
2022-08-15 03:12:57 +0600

    app update films

--- films/models.py
+++ films/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+import time
 
-# Create your models here.
+
+class film(models.Model):
+	title = models.CharField("Название")
+	description = models.TextField("Описание")
+	country = CharField("Страна")
+	year = models.IntegerField(
+		"Год",
+		min_value = 1895,
+		max_value = time.strftime('%Y\n', time.localtime()))
+	# здесь будет связь с моделью жанра
+	
+	def __str__(self):
+		pass
+
+	class Meta:
+		verbose_name = "фильм"
+		verbose_name_plural = "фильмы"
