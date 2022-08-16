737a34f04437e900e77f5efe02c3165e568d4685
Danil1024 <danaiek2003@mail.ru>
2022-08-16 17:32:56 +0600

    base.html

--- kinogo_zone/settings.py
+++ kinogo_zone/settings.py
@@ -119,9 +119,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
-MEDIA_URL = "media/"
+STATICFILES_DIRS = [
+   os.path.join(BASE_DIR, "static"),
+]
 
-MEDIA_URL = '/media/'
+MEDIA_URL = "media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 
